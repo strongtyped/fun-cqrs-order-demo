8a266a2fba9fee0d7601cd499e6d153027309617
Renato Cavalcanti <renato@strongtyped.io>
2016-10-24 22:46:00 +0200

    step 4: View Projection - OrderDetails

--- app/backend/OrderDetailsProjection.scala
+++ app/backend/OrderDetailsProjection.scala
@@ -6,6 +6,42 @@ import model.write.OrderProtocol._
 
 class OrderDetailsProjection(orderDetailsRepo: OrderDetailsRepo) extends SyncProjection {
 
-  def handleEventSync: HandleEventSync = PartialFunction.empty
+  def handleEventSync: HandleEventSync = {
+    case evt: OrderWasCreated   => created(evt)
+    case evt: ItemWasAdded      => itemAdded(evt)
+    case evt: ItemWasRemoved    => itemRemoved(evt)
+    case evt: OrderWasCancelled => cancelled(evt)
+    case evt: OrderWasPayed     => payed(evt)
+  }
+
+  def created(evt: OrderWasCreated): Unit =
+    orderDetailsRepo.save(OrderDetails(evt.number, Initiated))
+
+  def itemAdded(evt: ItemWasAdded): Unit =
+    orderDetailsRepo.updateById(evt.number) { ord =>
+      val updated = ItemDetails(evt.itemId, evt.name, evt.price) :: ord.items
+      ord.copy(items = updated, status = Open)
+    }
+
+  def itemRemoved(evt: ItemWasRemoved): Unit =
+    orderDetailsRepo.updateById(evt.number) { ord =>
+      val updatedItems = Lists.removeFirst(ord.items)(_.itemId == evt.itemId)
+
+      val status =
+        if (updatedItems.isEmpty) Initiated
+        else Open
+
+      ord.copy(items = updatedItems, status = status)
+    }
+
+  def cancelled(evt: OrderWasCancelled): Unit =
+    orderDetailsRepo.updateById(evt.number) { ord =>
+      ord.copy(status = Cancelled)
+    }
+
+  def payed(evt: OrderWasPayed): Unit =
+    orderDetailsRepo.updateById(evt.number) { ord =>
+      ord.copy(status = Payed)
+    }
   
 }
